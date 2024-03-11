a012302d1f1c981e2b469ffcac6c5c318d2ed1f5
Herschdorfer <herschdorfer@gmail.com>
2024-03-11 18:16:44 +0100

    Update EC constructor to include a third parameter

--- src/test/java/crypt_basics/ec/ECTest.java
+++ src/test/java/crypt_basics/ec/ECTest.java
@@ -12,7 +12,7 @@ class ECTest {
 
 	@Test
 	void testAddition1() {
-		EC ec = new EC(new BigInteger("97"), new BigInteger("10"));
+		EC ec = new EC(new BigInteger("97"), new BigInteger("10"), new BigInteger("3"));
 		ECPoint point1 = ec.new ECPoint(new BigInteger("23"), new BigInteger("9"));
 		ECPoint point2 = ec.new ECPoint(new BigInteger("23"), new BigInteger("9"));
 
@@ -21,10 +21,10 @@ class ECTest {
 		assertEquals(BigInteger.valueOf(33), result.getX());
 		assertEquals(BigInteger.valueOf(63), result.getY());
 	}
-	
+
 	@Test
 	void testAddition2() {
-		EC ec = new EC(new BigInteger("97"), new BigInteger("2"));
+		EC ec = new EC(new BigInteger("97"), new BigInteger("2"), new BigInteger("3"));
 		ECPoint point1 = ec.new ECPoint(new BigInteger("17"), new BigInteger("10"));
 		ECPoint point2 = ec.new ECPoint(new BigInteger("95"), new BigInteger("31"));
 
@@ -33,10 +33,10 @@ class ECTest {
 		assertEquals(BigInteger.valueOf(1), result.getX());
 		assertEquals(BigInteger.valueOf(54), result.getY());
 	}
-	
+
 	@Test
 	void testAddition_P1Inf() {
-		EC ec = new EC(new BigInteger("97"), new BigInteger("2"));
+		EC ec = new EC(new BigInteger("97"), new BigInteger("2"), new BigInteger("3"));
 		ECPoint point1 = ec.new ECPoint();
 		ECPoint point2 = ec.new ECPoint(new BigInteger("95"), new BigInteger("31"));
 
@@ -45,10 +45,10 @@ class ECTest {
 		assertEquals(BigInteger.valueOf(95), result.getX());
 		assertEquals(BigInteger.valueOf(31), result.getY());
 	}
-	
+
 	@Test
 	void testAddition_P2Inf() {
-		EC ec = new EC(new BigInteger("97"), new BigInteger("2"));
+		EC ec = new EC(new BigInteger("97"), new BigInteger("2"), new BigInteger("3"));
 		ECPoint point1 = ec.new ECPoint(new BigInteger("95"), new BigInteger("31"));
 		ECPoint point2 = ec.new ECPoint();
 
@@ -57,10 +57,10 @@ class ECTest {
 		assertEquals(BigInteger.valueOf(95), result.getX());
 		assertEquals(BigInteger.valueOf(31), result.getY());
 	}
-	
+
 	@Test
 	void testAddition_P1EqualP2() {
-		EC ec = new EC(new BigInteger("97"), new BigInteger("2"));
+		EC ec = new EC(new BigInteger("97"), new BigInteger("2"), new BigInteger("3"));
 		ECPoint point1 = ec.new ECPoint(new BigInteger("95"), new BigInteger("31"));
 		ECPoint point2 = ec.new ECPoint(new BigInteger("95"), new BigInteger("31"));
 
@@ -69,36 +69,33 @@ class ECTest {
 		assertEquals(BigInteger.valueOf(74), result.getX());
 		assertEquals(BigInteger.valueOf(77), result.getY());
 	}
-	
+
 	@Test
 	void testMultiplikation_n1() {
-		EC ec = new EC(new BigInteger("97"), new BigInteger("2"));
+		EC ec = new EC(new BigInteger("97"), new BigInteger("2"), new BigInteger("3"));
 		ECPoint point1 = ec.new ECPoint(new BigInteger("95"), new BigInteger("31"));
-		
 
 		ECPoint result = ec.scalarMultiplication(BigInteger.ONE, point1);
 
 		assertEquals(BigInteger.valueOf(95), result.getX());
 		assertEquals(BigInteger.valueOf(31), result.getY());
 	}
-	
+
 	@Test
 	void testMultiplikation_n2() {
-		EC ec = new EC(new BigInteger("97"), new BigInteger("2"));
+		EC ec = new EC(new BigInteger("97"), new BigInteger("2"), new BigInteger("3"));
 		ECPoint point1 = ec.new ECPoint(new BigInteger("95"), new BigInteger("31"));
-		
 
 		ECPoint result = ec.scalarMultiplication(BigInteger.TWO, point1);
 
 		assertEquals(BigInteger.valueOf(74), result.getX());
 		assertEquals(BigInteger.valueOf(77), result.getY());
 	}
-	
+
 	@Test
 	void testMultiplikation_n10() {
-		EC ec = new EC(new BigInteger("97"), new BigInteger("2"));
+		EC ec = new EC(new BigInteger("97"), new BigInteger("2"), new BigInteger("3"));
 		ECPoint point1 = ec.new ECPoint(new BigInteger("95"), new BigInteger("31"));
-		
 
 		ECPoint result = ec.scalarMultiplication(BigInteger.TEN, point1);
 
