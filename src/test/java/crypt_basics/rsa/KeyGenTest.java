baed344fa9e5618e6998d96e069f7db9dc5866ff
Herschdorfer <herschdorfer@gmail.com>
2024-03-17 20:54:36 +0100

    increase prime probability.

--- src/test/java/crypt_basics/rsa/KeyGenTest.java
+++ src/test/java/crypt_basics/rsa/KeyGenTest.java
@@ -18,7 +18,7 @@ class KeyGenTest {
 	@Test
 	void testRsaPrivateKeyGen() {
 		RSAKeyGen sut = new RSAKeyGen(BigInteger.probablePrime(8, new Random()),
-				BigInteger.probablePrime(8, new Random()), BigInteger.valueOf(65537));
+				BigInteger.probablePrime(100, new Random()), BigInteger.valueOf(65537));
 		BigInteger d = sut.generatePrivateKey();
 
 		char[] chars = "Hello World".toCharArray();
@@ -32,6 +32,7 @@ class KeyGenTest {
 		for (int i = 0; i < chars.length; i++) {
 			assertEquals(chars[i], (char) decrypted[i].intValue());
 		}
+
 	}
 
 }
